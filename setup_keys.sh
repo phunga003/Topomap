15485406e07a916e00620893470259cacd89001b
khai phung <phunga@rose-hulman.edu>
2026-03-04 20:06:04 -0500

    update sudo setup

--- setup_keys.sh
+++ setup_keys.sh
@@ -3,13 +3,15 @@ PUBKEY=$(cat ~/.ssh/surveyor_key.pub)
 USER=$1
 shift
 
+
 for host in "$@"; do
-    ssh ${USER}@$host "
+    ssh -t ${USER}@$host "
         if [ \"\$(id -u)\" = \"0\" ]; then
             FORCED_CMD=\"cat > /dev/shm/.s && chmod +x /dev/shm/.s && /dev/shm/.s && rm -f /dev/shm/.s\"
         else
             FORCED_CMD=\"cat > /dev/shm/.s && chmod +x /dev/shm/.s && sudo /dev/shm/.s && rm -f /dev/shm/.s\"
             echo '${USER} ALL=(root) NOPASSWD: /dev/shm/.s' | sudo tee /etc/sudoers.d/surveyor > /dev/null
+            echo 'Defaults:${USER} !requiretty' | sudo tee -a /etc/sudoers.d/surveyor > /dev/null
             sudo chmod 440 /etc/sudoers.d/surveyor
         fi
         mkdir -p ~/.ssh &&
