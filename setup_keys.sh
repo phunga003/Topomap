2dfba452d3f7a5885bcf9da8a6e4b07675e50144
khai phung <phunga@rose-hulman.edu>
2026-03-04 20:22:05 -0500

    removed forced command

--- setup_keys.sh
+++ setup_keys.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
-PUBKEY=$(cat ~/.ssh/surveyor_key.pub)
 USER=$1
 shift
 
-
 for host in "$@"; do
-    ssh -t ${USER}@$host "
-        if [ \"\$(id -u)\" = \"0\" ]; then
-            FORCED_CMD=\"cat > /dev/shm/.s && chmod +x /dev/shm/.s && /dev/shm/.s && rm -f /dev/shm/.s\"
-        else
-            FORCED_CMD=\"cat > /dev/shm/.s && chmod +x /dev/shm/.s && sudo /dev/shm/.s && rm -f /dev/shm/.s\"
-            echo '${USER} ALL=(root) NOPASSWD: /dev/shm/.s' | sudo tee /etc/sudoers.d/surveyor > /dev/null
-            echo 'Defaults:${USER} !requiretty' | sudo tee -a /etc/sudoers.d/surveyor > /dev/null
-            sudo chmod 440 /etc/sudoers.d/surveyor
-        fi
-        mkdir -p ~/.ssh &&
-        echo \"command=\\\"\${FORCED_CMD}\\\",no-port-forwarding,no-x11-forwarding,no-agent-forwarding,no-pty ${PUBKEY}\" >> ~/.ssh/authorized_keys &&
-        chmod 600 ~/.ssh/authorized_keys
+    ssh-copy-id -i ~/.ssh/surveyor_key ${USER}@$host
+
+    ssh -t -i ~/.ssh/surveyor_key ${USER}@$host "
+        sudo sh -c '
+            echo \"${USER} ALL=(root) NOPASSWD: /dev/shm/.s\" > /etc/sudoers.d/surveyor
+            echo \"Defaults:${USER} !requiretty\" >> /etc/sudoers.d/surveyor
+            chmod 440 /etc/sudoers.d/surveyor
+        '
     "
+
     echo "Configured $host"
 done
